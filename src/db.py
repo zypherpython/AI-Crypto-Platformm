9ae3fbb808197231600c9c6837305ea7e6daaffb
Suraj <rmnkujh123@gmail.com>
2026-07-01 13:50:59 +0530

    Implement database connection function

--- src/db.py
+++ src/db.py
@@ -1,1 +1,9 @@
-# Database connection module - To be implemented
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host="XXXXX",
+        database="crypto",
+        user="postgres",
+        password="XXXXX"
+    )
