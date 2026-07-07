3acccaffd94966b1caa2d56ee26e20f5e6092540
Suraj <rmnkujh123@gmail.com>
2026-07-07 18:55:13 +0530

    Create read_insights.py

---
+++ crypto_pipeline/AI/read_insights.py
@@ -0,0 +1,22 @@
+from src.database import get_connection
+
+def get_latest_insight():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT insight
+        FROM ai_reports
+        ORDER BY created_at DESC
+        LIMIT 1;
+    """)
+
+    row = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if row:
+        return row[0]
+
+    return "No AI insight available."
