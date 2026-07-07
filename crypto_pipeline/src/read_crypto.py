a8753feecb01cd64cbbf35b91f79ab01081f22c1
Suraj <rmnkujh123@gmail.com>
2026-07-07 18:43:13 +0530

    Update read_crypto.py

--- crypto_pipeline/src/read_crypto.py
+++ crypto_pipeline/src/read_crypto.py
@@ -1,1 +1,17 @@
-# Read crypto data for Streamlit
+from src.database import get_connection
+import pandas as pd
+
+
+def get_crypto_data():
+    conn = get_connection()
+
+    query = """
+    SELECT *
+    FROM crypto_prices;
+    """
+
+    df = pd.read_sql(query, conn)
+
+    conn.close()
+
+    return df
